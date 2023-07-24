39dcc245a4bb72cd089a4fc32beb7119034a4ba5
Shanto Islam <rajs45693@gmail.com>
2023-07-24 20:49:29 +0600

    start implement review section

--- src/Components/ResearchPaper/ResearchPaper.jsx
+++ src/Components/ResearchPaper/ResearchPaper.jsx
@@ -2,8 +2,9 @@
 
 const ResearchPaper = () => {
     return (
-        <div className="mt-20">
-            <h1 className="text-3xl text-center">Collage Recommending</h1>
+        <div className="mt-20 ">
+            <h1 className="text-3xl text-center">Collage Recommending  Research paper</h1>
+            <hr className="mt-5 w-1/2 mx-auto" />
             <h1 className="text-2xl link decoration-blue-500 m-10"><a href="https://digital.lib.washington.edu/researchworks/handle/1773/47830">Communications, Department of [14]</a></h1>
             <h1 className="text-2xl link decoration-blue-500 m-10"><a href="https://digital.lib.washington.edu/researchworks/handle/1773/2081">Comparative History of Ideas Program [20]</a></h1>
             <h1 className="text-2xl link decoration-blue-500 m-10"><a href="https://digital.lib.washington.edu/researchworks/handle/1773/17552">Disability Studies [1]</a></h1>
---
+++ src/Components/ReviewSection/ReviewPage.jsx
@@ -0,0 +1,11 @@
+
+
+const ReviewPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl">To Do</h1>
+        </div>
+    );
+};
+
+export default ReviewPage;
